cacbbb3745904b402c6a54bf6c6f7c1a2d12f0a5
a487czz <leochp@gmail.com>
2017-07-10 11:07:27 -0300

    finnish

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,43 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello></hello>
+    <todo-list v-bind:todos="todos"></todo-list>
+    <create-todo v-on:create-todo="createTodo"></create-todo>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import sweetalert from 'sweetalert'
+import TodoList from './components/TodoList' 
+import CreateTodo from './components/CreateTodo'
 
 export default {
   name: 'app',
   components: {
-    Hello
-  }
-}
+    TodoList,
+    CreateTodo,
+  },
+
+  data(){
+    return {
+        todos: [{
+          title: 'Todo A',
+          project: 'Project A',
+          done: false
+        },{
+          title: 'Todo B',
+          project: 'Project B',
+          done: true
+        }]
+    }
+  },
+  methods: {
+    createTodo(newTodo) {
+      this.todos.push(newTodo);
+      sweetalert('Success!', 'To-Do created!', 'success');
+    },
+  },
+};
 </script>
 
 <style>
